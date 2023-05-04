9d175c2e0bd0e9eee40713bb700af66691249d55
aliciamartindelpozo <alicmade@muyax.com>
2023-05-04 20:22:00 +0200

    final

--- src/main/java/AplicacionDeVenta/App/Application.java
+++ src/main/java/AplicacionDeVenta/App/Application.java
@@ -1,7 +1,12 @@
 package AplicacionDeVenta.App;
 
 import AplicacionDeVenta.Buttons.Ordenador;
+import AplicacionDeVenta.Factories.GUIFactory;
 
 public class Application {
     private Ordenador ordenador;
+
+    public Application(GUIFactory ordenador) {
+       // ordenador = factory.createOrdenador();
+    }
 }
